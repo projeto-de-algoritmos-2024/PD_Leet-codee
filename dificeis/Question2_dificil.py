dc8021247e6aeb005e3ad4c80be94f4609bd86bc
Harry_cmartin <harrysoncampos@gmail.com>
2025-02-09 14:38:59 -0300

    Additions

--- dificeis/Question2_dificil.py
+++ dificeis/Question2_dificil.py
@@ -4,3 +4,13 @@ class Solution:
         
         total = 0
         max_satisfaction = 0
+
+        for i in range(len(satisfaction) - 1, -1, -1):
+            total += satisfaction[i]
+            
+            if total + max_satisfaction <= max_satisfaction:
+                break
+            
+            max_satisfaction += total
+        
+        return max_satisfaction
